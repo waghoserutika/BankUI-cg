bf6a71d511aa67a6b33095fed330cd301f62aa74
Rutika Waghose <waghoserutika17@gmail.com>
2022-01-11 13:19:32 +0530

    login integrated

--- WebContent/WEB-INF/jsps/login.jsp
+++ WebContent/WEB-INF/jsps/login.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> 
+<%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,18 +15,24 @@
 		<div class="row">
 			<div class="col-lg-2"></div>
 			<div class="col-lg-8">
+			
+			<sf:form method="post" action="${pageContext.request.contextPath }/process-login" modelAttribute="user">
 				<div class="form-group">
 					<label class="form-label mt-4">LOGIN</label>
+					<div style="font-size:medium;  color: red" > <%=request.getAttribute("msg") %></div>
 					<div class="form-floating mb-3">
-						<input type="email" class="form-control" id="floatingInput"
-							placeholder="name@example.com"> <label
-							for="floatingInput">Email address</label>
+						<sf:input type="text" class="form-control" id="floatingInput"
+							placeholder="name@example.com" path="username" /> <label
+							for="floatingInput">Username</label>
 					</div>
 					<div class="form-floating">
-						<input type="password" class="form-control" id="floatingPassword"
-							placeholder="Password"> <label for="floatingPassword">Password</label>
+						<sf:input type="password" class="form-control" id="floatingPassword"
+							placeholder="Password" path="password" /> <label for="floatingPassword" >Password</label>
 					</div>
+					<br />
+					<button type="submit" class="btn btn-primary">LOGIN</button>
 				</div>
+				</sf:form>
 			</div>
 			<div class="col-lg-2"></div>
 		</div>
--- src/com/bankui/controller/AccountOpenController.java
+++ src/com/bankui/controller/AccountOpenController.java
@@ -1,6 +1,11 @@
 package com.bankui.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.bankui.beans.Customer;
+import com.bankui.beans.UserInfo;
 
 @Controller
 public class AccountOpenController {
 
 	@Autowired
 	private Customer customer; 
+	@Autowired
+	private UserInfo user; 
 	
 	@RequestMapping("/")
 	public String showAccountOpenScreen(Model model) {
@@ -22,11 +30,47 @@ public class AccountOpenController {
 	}
 	
 	@RequestMapping("/create-account")
-	public String showLoginScreen(@ModelAttribute Customer customer) {
+	public String showLoginScreen(@ModelAttribute Customer customer, Model model) {
 		//Call the API and pass this customer object 
 		String url ="http://localhost:8181/customer";
 		RestTemplate restTemplate = new RestTemplate(); 
 		Customer c = restTemplate.postForObject(url, customer, Customer.class);
+		model.addAttribute("user", user);
+		model.addAttribute("msg", "");
+		return "login";
+	}
+	
+	@RequestMapping("/process-login")
+	public String processLogin(@ModelAttribute UserInfo user, Model model) {
+		//generate encoded auth_code using username and password and attach it to headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth(user.getUsername(), user.getPassword());
+		
+		//attach this header to request Object 
+		HttpEntity<Object> request = new HttpEntity<>(headers); 
+		
+		//call the api
+		RestTemplate restTemplate = new RestTemplate();
+		String url="http://localhost:8181/user";
+		
+		try {
+			ResponseEntity<UserInfo> entity = restTemplate.exchange(url, HttpMethod.GET, request, UserInfo.class);
+			UserInfo u = entity.getBody();
+			model.addAttribute("user", u);
+			return "dashboard";
+		}
+		catch(Exception e) {
+			model.addAttribute("user", user);
+			model.addAttribute("msg", "Invalid Credentials"); 
+			return "login";
+		}
+		
+	}
+	
+	@RequestMapping("/login")
+	public String showLogin(Model model) {
+		model.addAttribute("user", user);
+		model.addAttribute("msg", ""); 
 		return "login";
 	}
 }
